e3a3c771bf016851ca930bf36abe528c89f26990
Sergey <litf@yandex.ru>
2023-02-24 18:31:15 +0300

    add if and else

---
+++ Example005_bool/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Type Username: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "masha")
+    {
+        Console.WriteLine("YEAPPY, IT IS MASHA!");
+    }
+else
+    {
+        Console.Write("You are not Masha, go AWAY,");
+        Console.WriteLine(username);
+    }
