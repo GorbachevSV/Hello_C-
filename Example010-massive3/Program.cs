c7be78ee4761e063053d817eab153bd0735c4a53
Sergey <litf@yandex.ru>
2023-02-28 22:44:05 +0300

    Delete

---
+++ Example010-massive3/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = {1,12,13,14,15,16,14,18};
+
+int n = array.Length;
+int find = 14;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; // прервать поиск
+    }
+
+    index++;
+}
